7658a08b71dcf479cb38b560502cd6c06340ad64
10Keshav <keshavananth06@gmail.com>
2026-08-05 00:30:59 +0530

    edited: lspconfig, added gopls

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,5 +1,5 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "clangd", "rust-analyzer", "jdtls" } -- rust-analyzer, pyright
+local servers = { "clangd", "rust-analyzer", "jdtls", "gopls" } -- pyright
 vim.lsp.enable(servers)
 -- read :h vim.lsp.config for changing options of lsp servers
